8f5dfc9db5c437286a742f248ceed82267d0646e
okbaAbdelghani <114340991+okbaAbdelghani@users.noreply.github.com>
2023-07-31 18:59:34 +0100

    spatie package installed and configured

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UploadController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,6 +37,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::resource('roles', [RoleController::class]);
+    // Route::resource('users', [UserController::class]);
 });
 
 require __DIR__.'/auth.php';
